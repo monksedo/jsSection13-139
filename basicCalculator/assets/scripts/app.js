73fa48e570a6b621973220cbe79e339e7d3c4d84
Chris Nguyen <chris@tnwebdev.com>
2020-02-24 18:49:29 -0800

    Add code comments to unconventional calculator app.js

--- basicCalculator/assets/scripts/app.js
+++ basicCalculator/assets/scripts/app.js
@@ -2,18 +2,22 @@
 // Badic unconventional calculator
 // 
 
+// Gobal variables
 const defaultResult = 0;
 let result = defaultResult;
 
+// get UI/user input data
 function getUserInput() {
     return parseFloat(userInput.value);
 }
 
+// Display data on UI after calculation
 function displayResult(initResult, operator, num) {
     const text = `${initResult} ${operator} ${num}`;
     outputResult(result, text);
 }
 
+// Add function
 function add() {
     const num = getUserInput();
     const initResult = result;
@@ -21,6 +25,7 @@ function add() {
     displayResult(initResult, '+', num);
 }
 
+// Subtract function
 function subtract() {
     const num = getUserInput();
     const initResult = result;
@@ -28,6 +33,7 @@ function subtract() {
     displayResult(initResult, '-', num);
 }
 
+// mulpiply function
 function multiply() {
     const num = getUserInput();
     const initResult = result;
@@ -35,6 +41,7 @@ function multiply() {
     displayResult(initResult, '*', num);
 }
 
+// division function
 function division() {
     const num = getUserInput();
     const initResult = result;
@@ -42,6 +49,7 @@ function division() {
     displayResult(initResult, '/', num);
 }
 
+// Event listener from UI elements
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
